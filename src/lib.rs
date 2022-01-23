5d01b772f98ab45b23dec25be487b71116febc5b
Colin Walters <walters@verbum.org>
2022-01-23 08:02:14 -0500

    Re-export `cap-std` and add a prelude
    To make this easier to use.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,15 @@
 #![deny(unsafe_code)]
 #![cfg_attr(feature = "dox", feature(doc_cfg))]
 
+// Re-export our dependencies
+pub use cap_std;
+
 #[cfg(not(windows))]
 pub mod cmdext;
 pub mod dirext;
+
+/// Prelude, intended for glob import.
+pub mod prelude {
+    pub use super::cmdext::CapStdExtCommandExt;
+    pub use super::dirext::CapStdExtDirExt;
+}
