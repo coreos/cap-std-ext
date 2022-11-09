282c23dbdcebeef02c8dc65cba3b16e571317ba0
Colin Walters <walters@verbum.org>
2022-11-09 08:27:34 -0500

    Bump to 1.0, matching cap-std
    This will end one major source of semver bumps for our reverse
    dependencies.
    
    For our API...it's not perfect, but it's working well enough
    and I think we can commit to it.

--- src/cmdext.rs
+++ src/cmdext.rs
@@ -2,8 +2,8 @@
 
 use cap_std::fs::Dir;
 use cap_tempfile::cap_std;
+use io_lifetimes::OwnedFd;
 use rustix::fd::{AsFd, FromRawFd, IntoRawFd};
-use rustix::io::OwnedFd;
 use std::os::unix::process::CommandExt;
 use std::sync::Arc;
 
@@ -21,7 +21,7 @@ impl CapStdExtCommandExt for std::process::Command {
     fn take_fd_n(&mut self, fd: Arc<OwnedFd>, target: i32) -> &mut Self {
         unsafe {
             self.pre_exec(move || {
-                let mut target = rustix::io::OwnedFd::from_raw_fd(target);
+                let mut target = OwnedFd::from_raw_fd(target);
                 rustix::io::dup2(&*fd, &mut target)?;
                 // Intentionally leak into the child.
                 let _ = target.into_raw_fd();
